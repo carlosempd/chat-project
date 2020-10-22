d7ff258b0c314796e09544d4f69a7a3fd825b7bd
Carlos <carlosempd@gmail.com>
2020-10-22 01:50:47 -0400

    add config var or DB in heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,10 @@
 /** 
  * Port
 */
-process.env.PORT = process.env.PORT || 3000; 
+process.env.PORT = process.env.PORT || 3000;
+
+/** 
+ * Database
+*/
+const urlDB = process.env.MONGO_URI || 'mongodb+srv://carlos:carlos123@cluster0.rhpb3.mongodb.net/chat?retryWrites=true&w=majority';
+process.env.MONGO_URI = urlDB;
--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.static(publicPath));
 module.exports.io = socketIO(server);
 require('./sockets/socket');
 
-mongoose.connect('mongodb+srv://carlos:carlos123@cluster0.rhpb3.mongodb.net/chat?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
